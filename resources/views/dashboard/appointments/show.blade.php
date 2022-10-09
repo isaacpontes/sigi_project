daae71c5d5a862d1f87f698cdf9fa37f79e27c63
Isaac Pontes <isaaclfp@outlook.com>
2022-10-09 17:48:55 -0300

    fix: update show appointment view

--- resources/views/dashboard/appointments/show.blade.php
+++ resources/views/dashboard/appointments/show.blade.php
@@ -1,17 +1,55 @@
 <x-app-layout>
     <x-slot name="header">
-        {{ __('Appointment') . " - " . $appointment->name }}
+        {{ __('Appointment') . ' - ' . $appointment->name }}
     </x-slot>
 
-    <div class="col-md-6">
-        <label>Nome: {{ $appointment->name }}</label><br>
-        <label>Data e Hora: {{ $appointment->happens_at }}</label><br>
-        <label>Outras Informações: {{ $appointment->add_info }}</label><br>
-        <hr>
-        <a href="{{ route('dashboard.appointments.index') }}">
-            <button type="button" class="btn btn-light float-right">Voltar</button>
-        </a>
+    <div class="card">
+        <div class="card-body">
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <h6>{{ __('Name') }}:</h6>
+                    <h5>{{ $appointment->name }}</h5>
+                </div>
+                <div class="col-md-4">
+                    <h6>{{ __('Date and Time') }}:</h6>
+                    <h5>{{ date('H:i - d/m/Y', strtotime($appointment->happens_at)) }}</h5>
+                </div>
+                <div class="col-md-4">
+                    <h6>{{ __('Finished') }}?</h6>
+                    @if ($appointment->completed)
+                        <span class="text-success">
+                            <i class="fas fa-check-circle fa-fw"></i>
+                        </span>
+                        <span class="fs-5">Sim</span>
+                    @else
+                        <span class="text-danger">
+                            <i class="fas fa-times-circle fa-fw"></i>
+                        </span>
+                        <span class="fs-5">Não</span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <h6>{{ __('Additional Information') }}:</h6>
+                    <h5>{{ $appointment->add_info }}</h5>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="mb-3 d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <a href="{{ url()->previous() }}">
+                        <button type="button" class="btn btn-primary">Voltar</button>
+                    </a>
+                    <a href="{{ route('dashboard.appointments.edit', $appointment->id) }}">
+                        <button type="button" class="btn btn-outline-secondary ms-1">Editar</button>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 
 </x-app-layout>
-
